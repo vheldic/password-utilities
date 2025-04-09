9807b32e735a1041a4256858bf3d7728d3b48033
Velid Heldic <velid2004@gmail.com>
2025-04-09 10:42:58 +0200

    Added more test cases

--- test/index.test.js
+++ test/index.test.js
@@ -1,6 +1,7 @@
 const { isStrongPassword, generateStrongPassword } = require('../src/index');
 
 test("Check if password is strong", () => {
+  expect(isStrongPassword("Short")).toBe(false);
   expect(isStrongPassword("Hello World")).toBe(false);
   expect(isStrongPassword("pC%mD8TpCKn2")).toBe(true);
 });
@@ -8,4 +9,5 @@ test("Check if password is strong", () => {
 test("Generate a strong password based on length", () => {
   expect(generateStrongPassword(1).length).toBe(1);
   expect(generateStrongPassword(12).length).toBe(12);
+  expect(generateStrongPassword().length).toBe(12);
 });
